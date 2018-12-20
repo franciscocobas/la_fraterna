45ad6d9e84693a2245e74d5882c13a16f80ecdfa
Francisco Cobas <fcarocena@gmail.com>
2018-12-20 15:33:09 -0300

    Deploy changes

--- compile_scss.bash
+++
@@ -1,1 +0,0 @@
-sass --watch website/scss/la_fraterna.scss website/css/la_fraterna_sass.css &
---
+++ js/la_fraterna.js
@@ -0,0 +1,23 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
+  }
+};
+
+$(document).ready(function(){
+
+  var form = getUrlParameter('form');
+
+  if (form == 'confirmed') {
+    $('#div-consulta').removeClass('d-none');
+  }
+});
